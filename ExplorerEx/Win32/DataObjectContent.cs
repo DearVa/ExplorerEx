fdd52c723bbaece637954b7879eb13a84a9a19db
Dear.Va <1328886154@qq.com>
2022-02-05 23:19:33 +0800

    添加缺失项

---
+++ ExplorerEx/Win32/DataObjectContent.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace ExplorerEx.Win32;
+
+public enum DataObjectType {
+	Unknown, File, Bitmap, Text, UnicodeText, Html
+}
+
+public class DataObjectContent {
+	public DataObjectType Type { get; }
+
+	public DataObject Data { get; }
+
+	public DataObjectContent(IDataObject dataObject) {
+		if (dataObject is DataObject data) {
+			Data = data;
+			var formats = data.GetFormats();
+			if (formats != null) {
+				foreach (var format in formats) {
+					switch (format) {
+					case "FileDrop":
+						Type = DataObjectType.File;
+						return;  // 拿到一种就返回
+					case "Bitmap":
+						Type = DataObjectType.Bitmap;
+						return;
+					case "Text":
+						Type = DataObjectType.Text;
+						return;
+					case "UnicodeText":
+						Type = DataObjectType.UnicodeText;
+						return;
+					case "Html":
+						Type = DataObjectType.Html;
+						return;
+					}
+				}
+			}
+		}
+	}
+}
